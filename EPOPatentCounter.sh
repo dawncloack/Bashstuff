14eb50fe57f4cc43eaaea55b7205cba70a0972ed
dawncloack <73593191+dawncloack@users.noreply.github.com>
2020-11-14 10:19:14 +0100

    Add files via upload

--- EPOPatentCounter.sh
+++ EPOPatentCounter.sh
@@ -49,7 +49,7 @@ declare -i fileCount=$(ls -1 *.csv 2>/dev/null | wc -l) # First we count the num
 if (( fileCount < 1 ))
     then
 	UsageMSG
-    echoerr "\n\nNo CSV files present."
+    	echoerr "\n\nNo CSV files present."
 	unset fileCount #This might seem unnecessary, but garbage collection is a good programming practice.
 	exit 1
 fi
@@ -61,21 +61,23 @@ if [[ -n $(echo $1 | grep -oE '[[:alpha:]]') ]] # If $1 is anything but numbers
 	then
 	UsageMSG
 	echoerr "\n\nBad input in initial year!"
+	unset fileCount
 	exit 1
 else
-	declare -ir initialYear=${1:-1900} #if the input input is correct, it is assigned, otherwise it defaults
+	declare -ir initialYear=${1:-1900} #if the input input is correct, it is assigned, if it's null it defaults.
 fi
 
-if (( $initialYear >= $finalYear )) # If the final year is smaller than the initial one, they are swapped
+if (( $initialYear >= $finalYear )) # If the final year is smaller than the initial one, error.
 	then
 	UsageMSG
 	echoerr "\n\nInvalid starting year."
+	unset fileCount
 	exit 1
 fi
 
 
-declare -ar yearSpace=($(seq --separator=' ' $initialYear $finalYear)) # This declaration makes a read only array with the list of years. This declaration creates an array for the values in each member of the array. Each year and values per year share the same array index
-declare -a valueSpace # This array will contain the hits per year
+declare -ar yearSpace=($(seq --separator=' ' $initialYear $finalYear)) # This declaration makes a read only array with the list of years. 
+declare -a valueSpace # This declaration creates an array for the hits in each year of the array. Each year and values per year share the same array index
 declare -a headerSpace # This array will contain some headers with the hits per search, to be inserted at the beginning
 
 fileCount=0
@@ -83,22 +85,18 @@ fileCount=0
 for nextFile in *.csv
 do
 
-headerSpace[fileCount]=$(cat $nextFile | sed -n '3p' | cut -f1 --delimiter=\;)
-# The third line of each CSV, containing the number of hits per search and the name of the search, is copied.
+	headerSpace[fileCount]=$(cat $nextFile | sed -n '3p' | cut -f1 --delimiter=\;) # The third line of each CSV, containing the number of hits per search and the name of the search, is copied.
+	(( fileCount++ ))
 
     	for nextHit in $(egrep -o '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}";"[[:digit:]]{4}' $nextFile | \
 	cut -f1 --delimiter=\; | \
 	cut -f1 --delimiter=- | \
 	awk -v AWKvar="$initialYear" '$1 >= AWKvar {print $1}')
 #This grep-cut-cut-awk command extracts the year of publication from an Espacenet CSV. It is based on the observation that the publication year is the only date followed immediately by another date (that of the first priority). Only years above the initial one are provided.
+    	
     	do
-    	debugVal=$((nextHit - initialYear))
-    	printf "(%s - %s = %s)\n" $nextHit $initialYear $debugVal
         (( valueSpace[((nextHit - initialYear))]++ )) #using the initial year as offset lets us avoid costly searches through the array for each hit.
-
     	done
-
-(( fileCount++ ))
 
 done <<< "$nextFile"
 
@@ -106,19 +104,19 @@ resultsFilename="results$$.csv" #The output file will be named results + the PID
 
 touch "$resultsFilename"
 
-{ # this entire block of code redirects output to results.csv.
+{ # this entire block of code redirects output to results$$.csv.
 printf "\"\";\"\";\"%s\"\n" "$(date)" # First, the date and time.
 
 for element in "${headerSpace[@]}" # Second, the header from the csv are copied. They contain the names of searches and the hits per search.
 do
-	printf "\"\";\"\";%s\n" "$element"
+	printf "\"\";\"\";%s\n" "$element" #To make it more pleasant visually the headers are copied in the third column.
 done
 
-printf "\"\";\"\";\"%s\"\n" "(total results, unfiltered by year)"
+printf "\"\";\"\";\"%s\"\n" "(total results, filtered by year)"
 
-echo "\"Year\";\"Number of patents\";\"\"" #This is the header of the .csv file
+echo "\"Year\";\"Number of patents\";\"\"" #This is the header of the significant part of the .csv file
 
-readonly writerMax=$(( $(date +"%Y") - $initialYear ))
+readonly writerMax=$(( $(date +"%Y") - $initialYear )) #The number of entries is the current date minus the initial date.
 writerCount=0
 
 while (( $writerCount <= $writerMax ))
